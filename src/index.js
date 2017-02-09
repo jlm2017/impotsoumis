7cb77fac433c5eafe4f1156abb5b518feb9273c5
Michel Mongkhoy <michel.mongkhoy@gmail.com>
2017-02-09 14:23:15 +0100

    Remove all files xD

--- src/index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import AppView from './views/AppView.jsx';
-
-ReactDOM.render(<AppView/>, document.getElementById('content'));
-document.body.classList.remove('loading');
