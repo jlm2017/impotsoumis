a9c7112516bca76e9b4fe63eaf5aa1bd3d53424c
Michel Mongkhoy <michel.mongkhoy@gmail.com>
2017-02-12 21:23:59 +0100

    Finalisation du calcul de l'IR et CSG actuels

--- impotsoumis/src/data/simulation/Simulation.js
+++ impotsoumis/src/data/simulation/Simulation.js
@@ -1,7 +1,7 @@
 import CurrentIR from './../legislative_parameters/CurrentIR'
 import CalculParTranche from './CalculParTranche'
 
-function Simulation(revenu_fiscal_ref, nbparts, couple) {
+function Simulation(revenu_fiscal_ref, nbparts, couple, csg_taux_plein, csg_taux_reduit) {
 
     var revenu_fiscal_ref_total = revenu_fiscal_ref.salarie + revenu_fiscal_ref.chomeur + revenu_fiscal_ref.retraite
     var plaf_QF = CurrentIR.plafond.qf.parDemiPart.value
@@ -17,6 +17,23 @@ function Simulation(revenu_fiscal_ref, nbparts, couple) {
     var valeurMin = 0
     var valeurMax = d20 * (1 + couple) - c20 * nbparts - plaf_QF * 2 * (nbparts - (1 + couple))
     var e20 = Math.max(valeurMin, valeurMax)
+
+    var f20 = c20 * nbparts + e20
+
+    var plaf_decote_celib = CurrentIR.plafond.decote.celibataire.value
+    var plaf_decote_couple = CurrentIR.plafond.decote.couple.value
+
+    var plafond = couple == 0 ?  plaf_decote_celib : plaf_decote_couple
+    var tx_decote = CurrentIR.tauxDecote.value
+
+    var g20 = Math.max(0, tx_decote * (plafond - f20))
+
+    var seuil_recouv = CurrentIR.seuilRecouvrement.value
+    var difference = f20 - g20
+    var h20 = difference >= seuil_recouv ? difference : 0
+
+    var i20 = csg_taux_plein + csg_taux_reduit
+    var j20 = h20 + i20
 
     return {
         "revenu": {
@@ -51,12 +68,12 @@ function Simulation(revenu_fiscal_ref, nbparts, couple) {
             "total": {
                 "avantDecote": {
                     "avecPlafonnementQF": {
-                        "value": -1
+                        "value": f20
                     }
                 }
             },
             "du": {
-                "value": -1
+                "value": h20
             }
         },
         "montant": {
@@ -67,16 +84,16 @@ function Simulation(revenu_fiscal_ref, nbparts, couple) {
             }
         },
         "decote": {
-            "value": -1
+            "value": g20
         },
-        "csgDu": {
+        "csg": {
             "du": {
-                "value": -1
+                "value": i20
             }
         },
         "total": {
             "du": {
-                "value": -1
+                "value": j20
             }
         }
     }
--- impotsoumis/src/data/simulation/Simulation.test.js
+++ impotsoumis/src/data/simulation/Simulation.test.js
@@ -40,19 +40,42 @@ describe('Le revenu imposable', () => {
 
 describe("L'impôt avant décote ", () => {
     describe("avec QF par part fiscale", () => {
-        var net = 2800,
-            retraite = 0,
-            chomage = 0,
-            couple = 0,
-            nbEnfants = 0
+        it('est correct', () => {
 
-        var userParams = UserParams(net, retraite, chomage, couple, nbEnfants)
-        var irParams = IRParams(userParams)
+            var net = 2800,
+                retraite = 0,
+                chomage = 0,
+                couple = 0,
+                nbEnfants = 0
 
-        var simulation = Simulation(irParams.revenu.fiscalDeReference, userParams.nbPartsFiscales.value)
-        assert.equal(Math.round(simulation.impot.avantDecote.avecQF.par.partFiscale.value, couple), 313)
+            var userParams = UserParams(net, retraite, chomage, couple, nbEnfants)
+            var irParams = IRParams(userParams)
+
+            var simulation = Simulation(irParams.revenu.fiscalDeReference, userParams.nbPartsFiscales.value)
+            assert.equal(Math.round(simulation.impot.avantDecote.avecQF.par.partFiscale.value, couple), 313)
+        });
     });
     describe("sans QF par part fiscale", () => {
+        it('est correct', () => {
+
+            var net = 2800,
+                retraite = 0,
+                chomage = 0,
+                couple = 0,
+                nbEnfants = 0
+
+            var userParams = UserParams(net, retraite, chomage, couple, nbEnfants)
+            var irParams = IRParams(userParams)
+
+            var simulation = Simulation(irParams.revenu.fiscalDeReference, userParams.nbPartsFiscales.value, couple)
+            assert.equal(Math.round(simulation.impot.avantDecote.avecQF.par.partFiscale.value), 313)
+        });
+    });
+});
+
+describe("Montant dépassement plafond du QF", () => {
+    it('est correct', () => {
+
         var net = 2800,
             retraite = 0,
             chomage = 0,
@@ -63,20 +86,93 @@ describe("L'impôt avant décote ", () => {
         var irParams = IRParams(userParams)
 
         var simulation = Simulation(irParams.revenu.fiscalDeReference, userParams.nbPartsFiscales.value, couple)
-        assert.equal(Math.round(simulation.impot.avantDecote.avecQF.par.partFiscale.value), 313)
+        assert.equal(Math.round(simulation.montant.depassement.plafondQF.value), 0)
     });
 });
 
-describe("Montant dépassement plafond du QF", () => {
-    var net = 2800,
-        retraite = 0,
-        chomage = 0,
-        couple = 0,
-        nbEnfants = 0
+describe("L'impôt total avant décote avec plafonnement du QF ", () => {
+    it('est correct', () => {
 
-    var userParams = UserParams(net, retraite, chomage, couple, nbEnfants)
-    var irParams = IRParams(userParams)
+        var net = 2800,
+            retraite = 0,
+            chomage = 0,
+            couple = 0,
+            nbEnfants = 0
 
-    var simulation = Simulation(irParams.revenu.fiscalDeReference, userParams.nbPartsFiscales.value, couple)
-    assert.equal(Math.round(simulation.montant.depassement.plafondQF.value), 0)
+        var userParams = UserParams(net, retraite, chomage, couple, nbEnfants)
+        var irParams = IRParams(userParams)
+
+        var simulation = Simulation(irParams.revenu.fiscalDeReference, userParams.nbPartsFiscales.value, couple)
+        assert.equal(Math.round(simulation.impot.total.avantDecote.avecPlafonnementQF.value), 313)
+    });
+});
+
+describe("Décote", () => {
+    it('est correcte', () => {
+
+        var net = 2800,
+            retraite = 0,
+            chomage = 0,
+            couple = 0,
+            nbEnfants = 0
+
+        var userParams = UserParams(net, retraite, chomage, couple, nbEnfants)
+        var irParams = IRParams(userParams)
+
+        var simulation = Simulation(irParams.revenu.fiscalDeReference, userParams.nbPartsFiscales.value, couple)
+        assert.equal(Math.round(simulation.decote.value), 0)
+    });
+});
+
+describe("Impôt dû", () => {
+    it('est correct', () => {
+
+        var net = 2800,
+            retraite = 0,
+            chomage = 0,
+            couple = 0,
+            nbEnfants = 0
+
+        var userParams = UserParams(net, retraite, chomage, couple, nbEnfants)
+        var irParams = IRParams(userParams)
+
+        var simulation = Simulation(irParams.revenu.fiscalDeReference, userParams.nbPartsFiscales.value, couple)
+        assert.equal(Math.round(simulation.impot.du.value), 313)
+    });
+});
+
+describe("CSG", () => {
+
+    it('est correcte', () => {
+
+        var net = 2800,
+            retraite = 0,
+            chomage = 0,
+            couple = 0,
+            nbEnfants = 0
+
+        var userParams = UserParams(net, retraite, chomage, couple, nbEnfants)
+        var irParams = IRParams(userParams)
+
+        var simulation = Simulation(irParams.revenu.fiscalDeReference, userParams.nbPartsFiscales.value, couple, irParams.csg.taux.plein.salarie, irParams.csg.taux.reduit.salarie)
+        assert.equal(Math.round(simulation.csg.du.value), 275)
+    });
+});
+
+describe("Total", () => {
+
+    it('is awesome', () => {
+
+        var net = 2800,
+            retraite = 0,
+            chomage = 0,
+            couple = 0,
+            nbEnfants = 0
+
+        var userParams = UserParams(net, retraite, chomage, couple, nbEnfants)
+        var irParams = IRParams(userParams)
+
+        var simulation = Simulation(irParams.revenu.fiscalDeReference, userParams.nbPartsFiscales.value, couple, irParams.csg.taux.plein.salarie, irParams.csg.taux.reduit.salarie)
+        assert.equal(Math.round(simulation.total.du.value), 588)
+    });
 });
