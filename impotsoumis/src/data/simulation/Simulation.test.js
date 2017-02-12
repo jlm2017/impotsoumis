b28cd631305bc4170bda90883f65a152c212b067
Michel Mongkhoy <michel.mongkhoy@gmail.com>
2017-02-12 20:57:10 +0100

    Format document

--- impotsoumis/src/data/simulation/Simulation.test.js
+++ impotsoumis/src/data/simulation/Simulation.test.js
@@ -41,42 +41,42 @@ describe('Le revenu imposable', () => {
 describe("L'impôt avant décote ", () => {
     describe("avec QF par part fiscale", () => {
         var net = 2800,
-                retraite = 0,
-                chomage = 0,
-                couple = 0,
-                nbEnfants = 0
+            retraite = 0,
+            chomage = 0,
+            couple = 0,
+            nbEnfants = 0
 
-            var userParams = UserParams(net, retraite, chomage, couple, nbEnfants)
-            var irParams = IRParams(userParams)
+        var userParams = UserParams(net, retraite, chomage, couple, nbEnfants)
+        var irParams = IRParams(userParams)
 
-            var simulation = Simulation(irParams.revenu.fiscalDeReference, userParams.nbPartsFiscales.value)
-            assert.equal(Math.round(simulation.impot.avantDecote.avecQF.par.partFiscale.value, couple), 313)
+        var simulation = Simulation(irParams.revenu.fiscalDeReference, userParams.nbPartsFiscales.value)
+        assert.equal(Math.round(simulation.impot.avantDecote.avecQF.par.partFiscale.value, couple), 313)
     });
     describe("sans QF par part fiscale", () => {
         var net = 2800,
-                retraite = 0,
-                chomage = 0,
-                couple = 0,
-                nbEnfants = 0
+            retraite = 0,
+            chomage = 0,
+            couple = 0,
+            nbEnfants = 0
 
-            var userParams = UserParams(net, retraite, chomage, couple, nbEnfants)
-            var irParams = IRParams(userParams)
+        var userParams = UserParams(net, retraite, chomage, couple, nbEnfants)
+        var irParams = IRParams(userParams)
 
-            var simulation = Simulation(irParams.revenu.fiscalDeReference, userParams.nbPartsFiscales.value, couple)
-            assert.equal(Math.round(simulation.impot.avantDecote.avecQF.par.partFiscale.value), 313)
+        var simulation = Simulation(irParams.revenu.fiscalDeReference, userParams.nbPartsFiscales.value, couple)
+        assert.equal(Math.round(simulation.impot.avantDecote.avecQF.par.partFiscale.value), 313)
     });
 });
 
 describe("Montant dépassement plafond du QF", () => {
-        var net = 2800,
-                retraite = 0,
-                chomage = 0,
-                couple = 0,
-                nbEnfants = 0
+    var net = 2800,
+        retraite = 0,
+        chomage = 0,
+        couple = 0,
+        nbEnfants = 0
 
-            var userParams = UserParams(net, retraite, chomage, couple, nbEnfants)
-            var irParams = IRParams(userParams)
+    var userParams = UserParams(net, retraite, chomage, couple, nbEnfants)
+    var irParams = IRParams(userParams)
 
-            var simulation = Simulation(irParams.revenu.fiscalDeReference, userParams.nbPartsFiscales.value, couple)
-            assert.equal(Math.round(simulation.montant.depassement.plafondQF.value), 0)
-    });
+    var simulation = Simulation(irParams.revenu.fiscalDeReference, userParams.nbPartsFiscales.value, couple)
+    assert.equal(Math.round(simulation.montant.depassement.plafondQF.value), 0)
+});
