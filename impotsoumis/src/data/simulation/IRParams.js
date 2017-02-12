8855695f8ff971d1ee89371d809dfb8b73afe248
Michel Mongkhoy <michel.mongkhoy@gmail.com>
2017-02-12 17:54:29 +0100

    CSG déductible

--- impotsoumis/src/data/simulation/IRParams.js
+++ impotsoumis/src/data/simulation/IRParams.js
@@ -1,7 +1,7 @@
 import CurrentCSG from './../legislative_parameters/CurrentCSG'
 import CurrentIR from './../legislative_parameters/CurrentIR'
 
-function _csg_tauxPlein_et_CRDS(sal_net, sal_brut, ret, alloc_cho, couple, nbparts) {
+function _csg_taux_plein_et_CRDS(sal_net, sal_brut, ret, alloc_cho, couple, nbparts) {
     var result = {
         "salarie": 0,
         "retraite": 0,
@@ -22,17 +22,72 @@ function _csg_tauxPlein_et_CRDS(sal_net, sal_brut, ret, alloc_cho, couple, nbpar
 
     result.salarie = (tx_sal_CSG + 0.005) * sal_brut * 0.9825
 
-    result.retraite = 0
     var revenusTotaux = sal_net + ret + alloc_cho
 
     if (revenusTotaux > (plaf_txred_celib + plaf_exo_demip * nbparts * 2)) {
         result.retraite = (tx_ret_CSG + 0.005) * ret
     }
 
-    result.chomeur = 0
-
     if (revenusTotaux > plaf_txred_celib_chom + plaf_txred_part_chom * nbparts * 4) {
         result.chomeur = (tx_chom_CSG + 0.005) * alloc_cho * (1 - (alloc_cho < plaf_abatchom) * abat_sal_CSG)
+    }
+
+    return result
+}
+
+function _csg_taux_reduit_et_CRDS(sal_net, sal_brut, ret, alloc_cho, couple, nbparts, csg_taux_plein_chomeur) {
+    var result = {
+        "salarie": 0,
+        "retraite": 0,
+        "chomeur": 0
+    }
+
+    var revenusTotaux = sal_net + ret + alloc_cho
+
+    var plaf_exo_demip = CurrentCSG.retraites.seuil.exoneration.demiPartSupplentaire.value
+    var plaf_txred_celib = CurrentCSG.retraites.seuil.taux.reduit.celibataire.value
+
+    var txred_CSG = CurrentCSG.retraites.seuil.taux.reduit.entierementDeductible.value
+
+    var seuil_exo_cho_celib = CurrentCSG.chomage.seuil.exoneration.celibataire.value
+    var seuil_exo_cho_demip = CurrentCSG.chomage.seuil.exoneration.parQuartSupplementaire.value
+    var plaf_abatchom = CurrentIR.plafond.abattement.chomage.value
+    var abat_sal_CSG = CurrentCSG.salaries.abattement.fraisPro.value
+
+    if (revenusTotaux <= plaf_txred_celib + plaf_exo_demip * nbparts * 2) {
+        result.retraite = (txred_CSG + 0.005) * ret
+    }
+
+    if (csg_taux_plein_chomeur <= 0) {
+        if (revenusTotaux > (seuil_exo_cho_celib + seuil_exo_cho_demip * nbparts * 4)) {
+            result.chomeur = (txred_CSG + 0.005) * alloc_cho * (1 - (alloc_cho < plaf_abatchom) * abat_sal_CSG)
+        }
+    }
+
+    return result
+}
+
+function _csg_deductible(sal_net, sal_brut, ret, alloc_cho, couple, nbparts, csg_taux_reduit_retraite, csg_taux_plein_chomeur) {
+    var result = {
+        "salarie": 0,
+        "retraite": 0,
+        "chomeur": 0
+    }
+
+    var txd_sal_CSG = CurrentCSG.salaries.tauxPlein.tauxDeductible.value
+    result.salarie = txd_sal_CSG * sal_brut * 0.9825
+
+    var txd_ret_CSG = CurrentCSG.retraites.tauxPlein.tauxDeductible.value
+    result.retraite = csg_taux_reduit_retraite > 0
+        ? csg_taux_reduit_retraite
+        : txd_ret_CSG * ret
+
+    var txred_CSG = CurrentCSG.retraites.seuil.taux.reduit.entierementDeductible.value
+    var plaf_abatchom = CurrentIR.plafond.abattement.chomage.value
+    var abat_sal_CSG = CurrentCSG.salaries.abattement.fraisPro.value
+
+    if (alloc_cho + csg_taux_plein_chomeur > 0) {
+        result.chomeur = txred_CSG * alloc_cho * (1 - (alloc_cho < plaf_abatchom) * abat_sal_CSG)
     }
 
     return result
@@ -46,17 +101,19 @@ function IRParams(userParams) {
     var couple = userParams.couple.value
     var nbParts = userParams.nbPartsFiscales.value
 
-    var csgTauxPlein = _csg_tauxPlein_et_CRDS(salaireNet, salaireBrut, retraite, chomage, couple, nbParts)
+    var csgTauxPlein = _csg_taux_plein_et_CRDS(salaireNet, salaireBrut, retraite, chomage, couple, nbParts)
+
+    var csgTauxReduit = _csg_taux_reduit_et_CRDS(salaireNet, salaireBrut, retraite, chomage, couple, nbParts, csgTauxPlein.chomeur)
+
+    var csgDeductible = _csg_deductible(salaireNet, salaireBrut, retraite, chomage, couple, nbParts, csgTauxReduit.retraite, csgTauxPlein.chomeur)
 
     return {
         "csg": {
             "taux": {
-                "plein": {
-                    "salarie": csgTauxPlein.salarie,
-                    "retraite": csgTauxPlein.retraite,
-                    "chomeur": csgTauxPlein.chomeur
-                }
-            }
+                "plein": csgTauxPlein,
+                "reduit": csgTauxReduit
+            },
+            "deductible": csgDeductible
         }
     }
 }
--- impotsoumis/src/data/simulation/IRParams.test.js
+++ impotsoumis/src/data/simulation/IRParams.test.js
@@ -7,10 +7,95 @@ var assert = require('assert');
 
 describe('IR Params', () => {
 
-    describe('#IRParams()', () => {
+    describe('# La CSG déductible', () => {
+it('pour un salarié doit être correcte', () => {
+            var net = 1500,
+                retraite = 500,
+                chomage = 1500,
+                couple = 1,
+                nbEnfants = 4
 
-        it('La CSG taux plein pour un salarié doit être correct', () => {
-            var net = 3000, retraite = 0, chomage = 0, couple = 1, nbEnfants = 2
+            var userParams = UserParams(net, retraite, chomage, couple, nbEnfants)
+            var irParams = IRParams(userParams)
+
+            assert.equal(Math.round(irParams.csg.deductible.salarie), 94)
+        });
+
+        it('pour un retraité doit être correcte', () => {
+            var net = 1500,
+                retraite = 500,
+                chomage = 1500,
+                couple = 1,
+                nbEnfants = 4
+
+            var userParams = UserParams(net, retraite, chomage, couple, nbEnfants)
+            var irParams = IRParams(userParams)
+
+            assert.equal(Math.round(irParams.csg.deductible.retraite), 22)
+        });
+
+        it('pour le chômeur doit être correcte', () => {
+            var net = 1500,
+                retraite = 500,
+                chomage = 1500,
+                couple = 1,
+                nbEnfants = 4
+
+            var userParams = UserParams(net, retraite, chomage, couple, nbEnfants)
+            var irParams = IRParams(userParams)
+
+            assert.equal(Math.round(irParams.csg.deductible.chomeur), 56)
+        });
+    });
+
+    describe('# La CSG taux réduit', () => {
+        it('pour un salarié doit être correcte', () => {
+            var net = 1500,
+                retraite = 500,
+                chomage = 1500,
+                couple = 1,
+                nbEnfants = 4
+
+            var userParams = UserParams(net, retraite, chomage, couple, nbEnfants)
+            var irParams = IRParams(userParams)
+
+            assert.equal(Math.round(irParams.csg.taux.reduit.salarie), 0)
+        });
+
+        it('pour un retraité doit être correcte', () => {
+            var net = 1500,
+                retraite = 500,
+                chomage = 1500,
+                couple = 1,
+                nbEnfants = 4
+
+            var userParams = UserParams(net, retraite, chomage, couple, nbEnfants)
+            var irParams = IRParams(userParams)
+
+            assert.equal(Math.round(irParams.csg.taux.reduit.retraite), 22)
+        });
+
+        it('pour le chômeur doit être correcte', () => {
+            var net = 1500,
+                retraite = 500,
+                chomage = 1500,
+                couple = 1,
+                nbEnfants = 4
+
+            var userParams = UserParams(net, retraite, chomage, couple, nbEnfants)
+            var irParams = IRParams(userParams)
+
+            assert.equal(Math.round(irParams.csg.taux.reduit.chomeur), 63)
+        });
+    });
+    describe('# La CSG Taux Plein', () => {
+
+        it('pour un salarié doit être correcte', () => {
+            var net = 3000,
+                retraite = 0,
+                chomage = 0,
+                couple = 1,
+                nbEnfants = 2
 
             var userParams = UserParams(net, retraite, chomage, couple, nbEnfants)
             var irParams = IRParams(userParams)
@@ -18,8 +103,12 @@ describe('IR Params', () => {
             assert.equal(Math.round(irParams.csg.taux.plein.salarie), 295)
         });
 
-        it('La CSG taux plein pour un retraité doit être correct', () => {
-            var net = 1500, retraite = 1500, chomage = 0, couple = 1, nbEnfants = 2
+        it('pour un retraité doit être correcte', () => {
+            var net = 1500,
+                retraite = 1500,
+                chomage = 0,
+                couple = 1,
+                nbEnfants = 2
 
             var userParams = UserParams(net, retraite, chomage, couple, nbEnfants)
             var irParams = IRParams(userParams)
@@ -27,30 +116,18 @@ describe('IR Params', () => {
             assert.equal(Math.round(irParams.csg.taux.plein.retraite), 107)
         });
 
-        it('La CSG taux plein pour au chômage doit être correct', () => {
-            var net = 1500, retraite = 0, chomage = 2000, couple = 1, nbEnfants = 0
+        it('pour le chômeur doit être correcte', () => {
+            var net = 1500,
+                retraite = 0,
+                chomage = 2000,
+                couple = 1,
+                nbEnfants = 0
 
             var userParams = UserParams(net, retraite, chomage, couple, nbEnfants)
             assert.equal(userParams.salaire.brut.value, 1875)
             var irParams = IRParams(userParams)
 
             assert.equal(Math.round(irParams.csg.taux.plein.chomeur), 132)
-        });
-
-        xit('C14-C16 - La CSG taux réduit et CRDS doivent être corrects', () => {
-            var net = 3000,
-                retraite = 0,
-                chomage = 0
-            var couple = 1,
-                nbEnfants = 1
-
-            var userParams = UserParams(net, retraite, chomage, couple, nbEnfants)
-            var irParams = IRParams(userParams)
-
-            assert.equal(irParams.csg.taux.reduit.salarie, 0)
-            assert.equal(irParams.csg.taux.reduit.retraite, 0)
-            assert.equal(irParams.csg.taux.reduit.chomeur, 0)
-
         });
 
         xit('D14-D16 - La CSG déductible doit être correcte', () => {
