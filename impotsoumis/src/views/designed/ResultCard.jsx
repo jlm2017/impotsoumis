85bf8e13ba7e35c3527423804833ce052f461d88
Michel Mongkhoy <michel.mongkhoy@gmail.com>
2017-02-18 14:15:25 +0100

    Remove code

--- impotsoumis/src/views/designed/ResultCard.jsx
+++ impotsoumis/src/views/designed/ResultCard.jsx
@@ -9,40 +9,7 @@ export default ({ color, left, right, title, total }) => {
   return (
     <div className={`ResultCard ${color}`}>
       <h3>{title}</h3>
-      <div className="amount">
-        <AnimatedNumber
-          format={(val) => numeral(val).format('€0,0')}
-          value={total}
-        />
-        €<span className="periodicity">/an</span>
-      </div>
-      <Chart
-        color={color}
-        left={left.value}
-        right={right.value}
-      />
-
-      <div className="legend">
-        <span className="left">
-          <span className="absolute">
-            <AnimatedNumber
-              format={(val) => numeral(val).format('€0,0')}
-              value={left.value}
-            />€
-          </span>
-          {left.legend}
-        </span>
-        <span className="right">
-          <span className="absolute">
-            <AnimatedNumber
-              format={(val) => numeral(val).format('€0,0')}
-              value={right.value}
-            />€
-          </span>
-          {right.legend}
-        </span>
-      </div>
-
+  
     </div>
   );
 };
