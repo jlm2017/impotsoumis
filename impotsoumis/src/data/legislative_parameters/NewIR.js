7a8f5aeef2c90863c1a38f53eccd749a5f2c42c9
Michel Mongkhoy <michel.mongkhoy@gmail.com>
2017-02-21 19:00:49 +0100

    Update New IR with tranche min/max

--- impotsoumis/src/data/legislative_parameters/NewIR.js
+++ impotsoumis/src/data/legislative_parameters/NewIR.js
@@ -5,77 +5,92 @@ const NewIR = {
             "name": "2%",
             "min": 0,
             "max": 200,
-            "taux": 0.02
+            "tauxMin": 0,
+            "tauxMax": 0.02
         }, {
             "name": "3%",
             "min": 200,
             "max": 500,
-            "taux": 0.03
+            "tauxMin": 0.02,
+            "tauxMax": 0.03
         }, {
             "name": "4%",
             "min": 500,
             "max": 800,
-            "taux": 0.04
+            "tauxMin": 0.03,
+            "tauxMax": 0.04
         }, {
             "name": "6%",
             "min": 800,
             "max": 1300,
-            "taux": 0.06
+            "tauxMin": 0.03,
+            "tauxMax": 0.06
         }, {
             "name": "8%",
             "min": 1300,
             "max": 1500,
-            "taux": 0.08
+            "tauxMin": 0.06,
+            "tauxMax": 0.08
         }, {
             "name": "10%",
             "min": 1500,
             "max": 1800,
-            "taux": 0.1
+            "tauxMin": 0.08,
+            "tauxMax": 0.1
         }, {
             "name": "12%",
             "min": 1800,
             "max": 2200,
-            "taux": 0.12
+            "tauxMin": 0.08,
+            "tauxMax": 0.12
         }, {
             "name": "14%",
             "min": 2200,
             "max": 3000,
-            "taux": 0.14
+            "tauxMin": 0.12,
+            "tauxMax": 0.14
         }, {
             "name": "18%",
             "min": 3000,
             "max": 4000,
-            "taux": 0.18
+            "tauxMin": 0.14,
+            "tauxMax": 0.18
         }, {
             "name": "25%",
             "min": 4000,
             "max": 4500,
-            "taux": 0.25
+            "tauxMin": 0.18,
+            "tauxMax": 0.25
         }, {
             "name": "30%",
             "min": 4500,
             "max": 5500,
-            "taux": 0.3
+            "tauxMin": 0.25,
+            "tauxMax": 0.3
         }, {
             "name": "40%",
             "min": 5500,
             "max": 10000,
-            "taux": 0.4
+            "tauxMin": 0.3,
+            "tauxMax": 0.4
         }, {
             "name": "50%",
             "min": 10000,
             "max": 25000,
-            "taux": 0.5
+            "tauxMin": 0.4,
+            "tauxMax": 0.5
         }, {
             "name": "60%",
             "min": 25000,
             "max": 33000,
-            "taux": 0.6
+            "tauxMin": 0.5,
+            "tauxMax": 0.6
         }, {
             "name": "90%",
             "min": 33000,
             "max": Number.MAX_SAFE_INTEGER,
-            "taux": 0.9
+            "tauxMin": 0.9,
+            "tauxMax": 0.9
         }
     ]
 }
