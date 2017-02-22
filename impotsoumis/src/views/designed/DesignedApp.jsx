c3af7111a12f8f255f78554400a25adf9447c66a
Chloé Gence <chloe.gence@gmail.com>
2017-02-22 16:43:29 +0100

    clean designedapp

--- impotsoumis/src/views/designed/DesignedApp.jsx
+++ impotsoumis/src/views/designed/DesignedApp.jsx
@@ -41,15 +41,14 @@ class DesignedApp extends Component {
 
     return (
       <Container className="DesignedApp">
-        <Row>
-          <Col>
-            <header>
-              10 secondes<br />
-              pour <strong>simuler</strong> votre imposition avec<br />
-              <h1>
-                La Révolution <strong>Fiscale</strong>
-              </h1>
-            </header>
+
+        <header>
+          10 secondes<br />
+          pour <strong>simuler</strong> votre imposition avec<br />
+          <h1>
+            La Révolution <strong>Fiscale</strong>
+          </h1>
+        </header>
 
         <Filters {...this.props} />
 
@@ -88,80 +87,87 @@ class DesignedApp extends Component {
               title={<span>Imposition <strong>actuelle</strong></span>}
               total={current.total}
               top={{
-                legend:
-                <span>Votre impôt sur le revenu est de :<br /><strong>
-                <AnimatedNumber
-                  format={(val) => ` ${numeral(val).format('€0,0')} `}
-                  value={current.IR}
-                /> €<sup>/an</sup></strong></span>,
+                legend: (
+                  <span>Votre impôt sur le revenu est de :<br />
+                  <strong>
+                  <AnimatedNumber
+                    format={(val) => ` ${numeral(val).format('€0,0')} `}
+                    value={current.IR}
+                  /> €<sup>/an</sup></strong></span>
+                ),
                 value: current.IR
               }}
               bottom={{
                 legend: (
-                  <span>Votre contribution sociale généralisée est de:<br /><strong>
+                  <span>Votre contribution sociale généralisée est de:<br />
+                  <strong>
                   <AnimatedNumber
                     format={(val) => ` ${numeral(val).format('€0,0')} `}
                     value={current.CSG}
                   /> €<sup>/an</sup></strong></span>
                 ),
                 value: current.CSG
-              }} />
-            </Col>
-            <Col sm={6} md={5}>
-              <ResultCard
-                color="blue"
-                title={<span>Avec la <strong>Révolution Fiscale</strong></span>}
-                total={revolution.total}
-                top={{
-                  legend: <span>Votre impôt sur le revenu sera de :<br /><strong>
+              }}
+            />
+          </Col>
+          <Col sm={6} md={5}>
+            <ResultCard
+              color="blue"
+              title={<span>Avec la <strong>Révolution Fiscale</strong></span>}
+              total={revolution.total}
+              top={{
+                legend: (
+                  <span>Votre impôt sur le revenu sera de :<br />
+                  <strong>
                   <AnimatedNumber
                     format={(val) => ` ${numeral(val).format('€0,0')} `}
                     value={revolution.IR}
-                  /> €<sup>/an</sup></strong></span>,
-                  value: revolution.IR
-                }}
-                bottom={{
-                  legend:
-                  <span>Votre contribution sociale généralisée sera de:<br /> <strong>
-                    <AnimatedNumber
-                      format={(val) => ` ${numeral(val).format('€0,0')} `}
-                      value={revolution.CSG}
-                    /> €<sup>/an</sup></strong></span>,
-                    value: revolution.CSG
-                  }}
-                />
-              </Col>
-            </Row>
-
-            <br/>
-            <br/>
-            <br/>
-            <Pulse icon={<Info />} />
-            <p>Ce simulateur vous permet d'expérimenter la Révolution Fiscale que nous proposons.<br/>C'est un outil pédagogique qui n'a pas vocation à être exact à l'euro près. </p>
-
+                  /> €<sup>/an</sup></strong></span>
+                ),
+                value: revolution.IR
+              }}
+              bottom={{
+                legend: (
+                  <span>Votre contribution sociale généralisée sera de:<br />
+                  <strong>
+                  <AnimatedNumber
+                    format={(val) => ` ${numeral(val).format('€0,0')} `}
+                    value={revolution.CSG}
+                  /> €<sup>/an</sup></strong></span>
+                ),
+                value: revolution.CSG
+              }}
+            />
           </Col>
-          <h2>Developer Data</h2>
-            <p>[Calcul Nouvel Impot Citoyen]<br/>R = {developer.sommeIR.revenu}.<br/>t = {developer.sommeIR.taux}.<br/>R * t = {developer.sommeIR.total}. <br/> R * t * 12 = {developer.sommeIR.total * 12}.</p>
-            <p>[Calcul Nouvelle CSG]<br/>R = {developer.sommeCSG.revenu}.<br/>t = {developer.sommeCSG.taux}.<br/>R*t = {developer.sommeCSG.total}.<br/> R * t * 12 = {developer.sommeCSG.total * 12}.</p>
-            <hr/>
-            <p>[A24] : Revenu imposable par part fiscale: {developer.a24}</p>
-            <p>Nouvel Impot Citoyen = Nouveau IR + Nouvelle CSG  : {developer.sommeIR.total}</p>
-            <p>Dont Nouveau IR : {developer.sommeIR.total - developer.sommeCSG.total}</p>
-            <p>Dont Nouveau CSG : {developer.sommeCSG.total}</p>
-
-            <p>[B24] : Impot par part fiscale avant CI QF (avec soustraction CSG) {developer.b24}</p>
-            <p>[C24] : Impot total : {developer.c24}</p>
-            <p>[D24] : Impot après CI QF : {developer.d24}</p>
-            <p>[E24] : Impot du après seuil recouvrement : {developer.e24}</p>
-            <h2>Version<br/>
-            <span className="laRevolution">0.1</span><br/>
-            <span className="fiscale">24</span>
-            </h2>
         </Row>
 
+        <div className="disclaimer">
+          <Pulse icon={<Info />} />
+          <p>Ce simulateur vous permet d'expérimenter la Révolution Fiscale que nous proposons.<br/>C'est un outil pédagogique qui n'a pas vocation à être exact à l'euro près. </p>
+        </div>
+
+        <div>
+          <h2>Developer Data</h2>
+          <p>[Calcul Nouvel Impot Citoyen]<br/>R = {developer.sommeIR.revenu}.<br/>t = {developer.sommeIR.taux}.<br/>R * t = {developer.sommeIR.total}. <br/> R * t * 12 = {developer.sommeIR.total * 12}.</p>
+          <p>[Calcul Nouvelle CSG]<br/>R = {developer.sommeCSG.revenu}.<br/>t = {developer.sommeCSG.taux}.<br/>R*t = {developer.sommeCSG.total}.<br/> R * t * 12 = {developer.sommeCSG.total * 12}.</p>
+          <hr/>
+          <p>[A24] : Revenu imposable par part fiscale: {developer.a24}</p>
+          <p>Nouvel Impot Citoyen = Nouveau IR + Nouvelle CSG  : {developer.sommeIR.total}</p>
+          <p>Dont Nouveau IR : {developer.sommeIR.total - developer.sommeCSG.total}</p>
+          <p>Dont Nouveau CSG : {developer.sommeCSG.total}</p>
+
+          <p>[B24] : Impot par part fiscale avant CI QF (avec soustraction CSG) {developer.b24}</p>
+          <p>[C24] : Impot total : {developer.c24}</p>
+          <p>[D24] : Impot après CI QF : {developer.d24}</p>
+          <p>[E24] : Impot du après seuil recouvrement : {developer.e24}</p>
+          <h2>Version<br/>
+          <span className="laRevolution">0.1</span><br/>
+          <span className="fiscale">24</span>
+        </h2>
+      </div>
       </Container>
     );
   }
 };
 
-    export default DesignedApp;
+export default DesignedApp;
