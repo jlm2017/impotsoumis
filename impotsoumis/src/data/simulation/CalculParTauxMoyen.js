2cc328d87012a3bc132360fb70e649e466044f81
Michel Mongkhoy <michel.mongkhoy@gmail.com>
2017-02-23 08:03:36 +0100

    Fix calcul par taux moyen

--- impotsoumis/src/data/simulation/CalculParTauxMoyen.js
+++ impotsoumis/src/data/simulation/CalculParTauxMoyen.js
@@ -12,22 +12,7 @@ function CalculParTauxMoyen(revenu, tranches) {
 
     var currentTranche = {}
     for (var tranche of tranches) {
-        if (revenu === tranche.min) {
-            return {
-                "revenu": revenu,
-                "taux": tranche.tauxMin,
-                "total": revenu * tranche.tauxMin
-            }
-        }
-
-        if (revenu === tranche.max) {
-            return {
-                "revenu": revenu,
-                "taux": tranche.tauxMax,
-                "total": revenu * tranche.tauxMax
-            }
-        }
-        if (tranche.min < revenu && revenu < tranche.max) {
+        if (tranche.min <= revenu && revenu < tranche.max) {
             if (tranche.max === Number.MAX_SAFE_INTEGER) {
                 return _calcul_derniereTranche_taux_marginal(revenu, tranche)
             } else {
--- impotsoumis/src/data/simulation/TauxEffectif.js
+++ impotsoumis/src/data/simulation/TauxEffectif.js
@@ -1,5 +1,10 @@
 function TauxEffectif(revenu, tranche) {
-    var lambda = (revenu - tranche.min) / (tranche.max - tranche.min)
+    var lambda = 0
+    if(tranche.min<=revenu) {
+        if(revenu<tranche.max) {
+            lambda = 1-(revenu-tranche.min)/(tranche.max-tranche.min)
+        }
+    }
     var result = lambda * tranche.tauxMin + (1 - lambda) * tranche.tauxMax
 
     return result
